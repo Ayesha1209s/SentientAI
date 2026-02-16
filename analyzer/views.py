58c88dade8c81f1927844fea1ed8cde1f7350ad3
Ayesha1209s <ummatulayesha4@gmail.com>
2026-02-16 21:10:11 +0530

    connection successful

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 def home(request):
     return render(request, "index.html")
+
+def predict(request):
+    if request.method == "POST":
+        text = request.POST.get("text")
+
+        return JsonResponse({"sentiment": "Testing OK"})
